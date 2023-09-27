2cffb30765a4474cb4ef2ec1162e6f1974282162
JA_RON <78304954+Jaron-Wilson@users.noreply.github.com>
2023-09-27 08:17:03 -0400

    PA1 - Restaurant Food Ordering System

---
+++ RFOS-3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/* 
+ * Author: 
+ * Date:
+ * 
+ * Programming Assigment xxxx 
+ * Due x/xx
+ * 
+ * Program Description: 
+ * 
+ * Honor Code: <replace this text with the honor code - one or two sentences>
+ * 
+ */
+
+public class RFOS{
+ 
+  public static void main(String args[]){
+    
+    // your code and comments go here
+  
+  }
+  
+}
