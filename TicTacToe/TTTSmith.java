c26441e46b6b6bb1861b1fcd61a21e39314f9945
JA_RON <78304954+Jaron-Wilson@users.noreply.github.com>
2023-11-03 11:29:02 -0400

    UPDATED FILES

--- TicTacToe/TTTSmith.java
+++ TicTacToe/TTTSmith.java
@@ -5,38 +5,36 @@ import java.util.regex.Pattern;
 
 public class TTTSmith {
     public static boolean isYourTurn = true;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String grid[][] = new String[3][3];
-        
+
         initBoard(grid, scanner);
-        
+
     }
 
     public static void yourTurn(String grid[][], Scanner scanner) {
         String xY = scanner.nextLine();
         if (xY.contains(",")) {
-            // Split it.
-            String[] parts = xY.split(Pattern.quote(","));
-            System.out.println(parts[0]);
-            String xCord = parts[0];
-            if(xCord == "1") {
-                gameBoard(grid, 1,0);
-
-            }
-        }else {
-            System.out.println("Please Give me a cordanate like: 1,2");
+            // // Split it.
+            // String[] parts = xY.split(Pattern.quote(","));
+            // // System.out.println(parts[0]);
+            // int yPos = (int) parts[1].toString();
+            // int xPos = (int) parts[0].toString();
+            // gameBoard(grid,xPos, yPos);
+            
+        } else {
+            System.out.println("Please give me a cordanate like: 1,2");
         }
     }
-
-
 
     private static void gameBoard(String[][] grid, int i, int j) {
         grid[i][j] = "x";
         
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                grid[i][j] = " ";
+                grid[i][j] = "X";
                 System.out.print("|" + grid[i][j]);
                 
         }
@@ -45,21 +43,20 @@ public class TTTSmith {
 
     public static void initBoard(String grid[][], Scanner scanner) {
         System.out.println("Let's begin!");
-        
+
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 grid[i][j] = " ";
                 System.out.print("|" + grid[i][j]);
-                
-        }
-        System.out.println("|");
-    }
-    if(getIsYourTurn()) {
-    System.out.println("Please enter coordinate of your location (x):");
-        yourTurn(grid, scanner);
-    }
-    }
 
+            }
+            System.out.println("|");
+        }
+        if (getIsYourTurn()) {
+            System.out.println("Please enter coordinate of your location (x):");
+            yourTurn(grid, scanner);
+        }
+    }
 
     public static boolean getIsYourTurn() {
         return isYourTurn;
