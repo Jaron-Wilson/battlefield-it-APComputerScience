af2ae6e26c5f402faaa60430a3ef5f0b4dac7f42
JA_RON <78304954+Jaron-Wilson@users.noreply.github.com>
2024-02-06 18:57:01 -0500

    Delete Minesweeper directory

--- Minesweeper/TestMinesweeper.java
+++
@@ -1,7 +0,0 @@
-public class TestMinesweeper {
-    public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper(10, 10, 10);
-        System.out.println(minesweeper.getNumMines() + " have been placed around randomly.");
-        minesweeper.playGame();
-    }
-}
